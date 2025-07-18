1fe9350f4a1a4c51435f47aebd59e43388527349
Muskan Tarafder <160621524+Muskan-Tarafder@users.noreply.github.com>
2025-07-19 01:37:44 +0530

    Update login.js

--- PandM/Frontend/static/js/login.js
+++ PandM/Frontend/static/js/login.js
@@ -11,7 +11,7 @@ async function validateLogin() {
 
     try {
         console.log("added")
-        const response = await fetch("http://127.0.0.1:5000/Enter", {
+        const response = await fetch("/Enter", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password })
